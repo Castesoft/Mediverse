4af50699760089d5e360cfeecfd8b3594a5d6fa8
rcastellanos-castesoft <rcastellanos@castesoft.com>
2025-01-06 13:33:41 -0600

    Enhance form components by adding typeahead control, updating multiselect and select fields, and refining doctor schedule constants for improved validation and user experience

--- client/src/app/_models/doctorSchedules/doctorScheduleConstants.ts
+++ client/src/app/_models/doctorSchedules/doctorScheduleConstants.ts
@@ -5,7 +5,7 @@ import { FormInfo } from "src/app/_models/forms/formTypes";
 
 
 export const doctorScheduleFormInfo: FormInfo<DoctorSchedule> = {
-  clinic: { type: 'typeahead', label: 'Clínica', showCodeSpan: false, },
+  clinic: { type: 'typeahead', label: 'Clínica', showCodeSpan: false, showLabel: false, validators: [Validators.required] },
 
   dateFrom: { type: 'date', label: 'Fecha desde', validators: [Validators.required] },
   dateTo: { type: 'date', label: 'Fecha hasta', validators: [Validators.required] },
@@ -18,7 +18,7 @@ export const doctorScheduleFormInfo: FormInfo<DoctorSchedule> = {
   hasPatientInformationAccess: { type: 'checkbox', label: 'Acceso a información del paciente' },
   medicalInsuranceCompany: { type: 'typeahead', label: 'Compañía de seguros médicos', showCodeSpan: false, },
   paymentMethodType: { type: 'typeahead', label: 'Tipo de método de pago', showCodeSpan: false, },
-  service: { type: 'typeahead', label: 'Servicio', showCodeSpan: false, },
+  service: { type: 'typeahead', label: 'Servicio', showCodeSpan: false, showLabel: false, validators: [Validators.required] },
 
   stripePaymentMethodId: { type: 'text', label: 'ID del método de pago de Stripe' },
 } as FormInfo<DoctorSchedule>;
