b700b44ef4777f6bdb2045e65026f7f28809ae23
rcastellanos-castesoft <rcastellanos@castesoft.com>
2025-03-10 14:12:36 -0600

    Refactor responsive design for doctor results and schedule windows; update mobile query handling and improve layout

--- client/src/app/search/windows/doctor-results-window.component.ts
+++ client/src/app/search/windows/doctor-results-window.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { Component, effect, HostBinding, inject } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { MobileQueryService } from 'src/app/_services/mobile-query.service';
 import { SearchService } from "src/app/_services/search.service";
+import { LogoIconComponent } from 'src/app/_shared/components/logo-icon/logo-icon.component';
 import { TablesModule } from "src/app/_shared/template/components/tables/tables.module";
 import { LoadingPlaceholderComponent } from "src/app/search/components/loading-placeholder.component";
 import { NoResultScreenComponent } from "src/app/search/components/no-result-screen.component";
@@ -9,7 +11,6 @@ import { ResultRowComponent } from "src/app/search/components/result-row.compone
 import { SearchFormComponent } from "src/app/search/components/search-form.component";
 
 @Component({
-  host: { class: 'd-flex flex-column position-relative', },
   selector: 'div[doctorResultsWindow]',
   templateUrl: './doctor-results-window.component.html',
   styleUrl: './doctor-results-window.component.scss',
@@ -22,8 +23,29 @@ import { SearchFormComponent } from "src/app/search/components/search-form.compo
     LoadingPlaceholderComponent,
     TablesModule,
     RouterModule,
+    LogoIconComponent,
   ],
 })
 export class DoctorResultsWindowComponent {
-  service: SearchService = inject(SearchService);
+  readonly service: SearchService = inject(SearchService);
+  readonly query = inject(MobileQueryService);
+
+  class = 'd-flex flex-column position-relative';
+
+  @HostBinding('class') get hostClass() {
+    return this.class;
+  }
+
+  constructor() {
+    effect(() => {
+
+      if (this.query.isMobile() === true) {
+        this.class = 'd-flex flex-column position-relative mobile-view';
+      } else if (this.query.isMobile() === false) {
+        this.class = 'd-flex flex-column position-relative desktop-view';
+      }
+
+    });
+  }
+
 }
--- client/src/app/search/windows/doctor-schedule-window.component.ts
+++ client/src/app/search/windows/doctor-schedule-window.component.ts
@@ -1,23 +1,24 @@
-import { CommonModule } from "@angular/common";
-import { Component, effect, inject, model, ModelSignal, OnInit, signal, WritableSignal } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
-import { ActivatedRoute, Router, RouterModule } from "@angular/router";
-import { Forms2Module } from "src/app/_forms2/forms-2.module";
-import { AvailableDay } from "src/app/_models/availableDay";
+import { CommonModule } from '@angular/common';
+import { Component, effect, HostBinding, inject, model, ModelSignal, OnInit, signal, WritableSignal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Forms2Module } from 'src/app/_forms2/forms-2.module';
+import { AvailableDay } from 'src/app/_models/availableDay';
 import { AvailableTime } from 'src/app/_models/availableTime';
-import { DoctorScheduleForm } from "src/app/_models/doctorSchedules/doctorScheduleForm";
+import { DoctorScheduleForm } from 'src/app/_models/doctorSchedules/doctorScheduleForm';
 import Event from 'src/app/_models/events/event';
 import { BadRequest } from 'src/app/_models/forms/badRequest';
 import { Search } from 'src/app/_models/search/search';
 import { getSearchRouteQueryParams } from 'src/app/_models/search/searchUtils';
-import { AccountService } from "src/app/_services/account.service";
+import { AccountService } from 'src/app/_services/account.service';
 import { DevService } from 'src/app/_services/dev.service';
-import { SearchService } from "src/app/_services/search.service";
-import { MaterialModule } from "src/app/_shared/material.module";
-import { SignInBasicFormComponent } from "src/app/auth/components/sign-in-basic-form.component";
-import { StepperIconComponent } from "src/app/search/utils/stepper-icon.component";
-import { FaIconComponent } from "@fortawesome/angular-fontawesome";
-import { IconsService } from "src/app/_services/icons.service";
+import { SearchService } from 'src/app/_services/search.service';
+import { MaterialModule } from 'src/app/_shared/material.module';
+import { SignInBasicFormComponent } from 'src/app/auth/components/sign-in-basic-form.component';
+import { StepperIconComponent } from 'src/app/search/utils/stepper-icon.component';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { IconsService } from 'src/app/_services/icons.service';
+import { MobileQueryService } from 'src/app/_services/mobile-query.service';
 
 @Component({
   selector: 'div[doctorScheduleWindow]',
@@ -36,17 +37,17 @@ import { IconsService } from "src/app/_services/icons.service";
   ],
 })
 export class DoctorScheduleWindowComponent implements OnInit {
-  private router: Router = inject(Router);
-  private route: ActivatedRoute = inject(ActivatedRoute);
-  accountService: AccountService = inject(AccountService);
-  service: SearchService = inject(SearchService);
-  icons: IconsService = inject(IconsService);
-  dev: DevService = inject(DevService);
+  private readonly router: Router = inject(Router);
+  private readonly route: ActivatedRoute = inject(ActivatedRoute);
+  readonly accountService: AccountService = inject(AccountService);
+  readonly service: SearchService = inject(SearchService);
+  readonly icons: IconsService = inject(IconsService);
+  readonly dev: DevService = inject(DevService);
+  readonly query = inject(MobileQueryService);
 
   selectedSchedule: ModelSignal<AvailableDay | null> = model.required();
   selectedTime: ModelSignal<AvailableTime | null> = model.required();
   scheduleWindowOpen: ModelSignal<boolean> = model.required();
-  isMobile: ModelSignal<boolean> = model.required();
 
   event: WritableSignal<Event | null> = signal(null);
   eventId: WritableSignal<number | null> = signal(null);
@@ -54,8 +55,21 @@ export class DoctorScheduleWindowComponent implements OnInit {
   form: DoctorScheduleForm = new DoctorScheduleForm();
   isSubmitting: boolean = false;
 
+  @HostBinding('class') get hostClass() {
+    return this.class;
+  }
+
+  class = '';
+
   constructor() {
     effect(() => {
+
+      if (this.query.isMobile() === true) {
+        this.class = 'mobile-view';
+      } else if (this.query.isMobile() === false) {
+        this.class = 'desktop-view';
+      }
+
       if (this.accountService.current()) {
         this.form
           .setDoctorResult(this.service.selected())
